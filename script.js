54ea09137e59729e165f561aeb09b8ed2f0b0c9c
Ivana Janostikova <ivkagre@gmail.com>
2024-09-30 20:15:21 +0200

    Pokracovanie

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ let pocetPredstaveni = 15;
 let prijem = cenaListek * pocetNavstevniku * pocetPredstaveni
 document.body.innerHTML += "Mesacny prijem je " + prijem + " EUR";
 
-let cenaListekStudent = 0.35 * 12;
+let cenaListekStudent = 0.65 * 12;
 
 document.body.innerHTML += "Studentsky listek stoji " + cenaListekStudent + " EUR";
+
+let pocetNavstevnikuStudentu = 0.4 * 174;
+
+document.body.innerHTML += "Pocet navstevniku z rad studentu je " + pocetNavstevnikuStudentu;
+
+let prijemStudenti = cenaListekStudent * pocetNavstevnikuStudentu; 
+
+document.body.innerHTML += "Prijem za studenty je " + prijemStudenti;
 
 
