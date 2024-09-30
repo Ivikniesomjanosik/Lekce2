d5def25744a2a642e0a3dc0424d712c9697268ca
Ivana Janostikova <ivkagre@gmail.com>
2024-09-30 20:01:36 +0200

    Cena studentskeho listka

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ let pocetPredstaveni = 15;
 let prijem = cenaListek * pocetNavstevniku * pocetPredstaveni
 document.body.innerHTML += "Mesacny prijem je " + prijem + " EUR";
 
+let cenaListekStudent = 0.35 * 12;
+
+document.body.innerHTML += "Studentsky listek stoji " + cenaListekStudent + " EUR";
+
 
