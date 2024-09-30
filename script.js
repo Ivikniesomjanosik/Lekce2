769e9cae4a3ef382578e6582d4c7d0c037b07ac5
Ivana Janostikova <ivkagre@gmail.com>
2024-09-30 19:56:41 +0200

    Pridavam prvy vysledok

---
+++ script.js
@@ -0,0 +1,8 @@
+let cenaListek = 12;
+let pocetNavstevniku = 174;
+let pocetPredstaveni = 15;
+
+let prijem = cenaListek * pocetNavstevniku * pocetPredstaveni
+document.body.innerHTML += "Mesacny prijem je " + prijem + " EUR";
+
+
